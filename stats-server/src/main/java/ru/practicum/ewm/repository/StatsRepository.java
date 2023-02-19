410c2965975cefdb8e4cf956395b6f3405aaf282
STravinJ <stravinpack@yandex.ru>
2023-02-19 05:50:36 +0300

    fix: исправление ошибок при тестировании

--- stats-server/src/main/java/ru/practicum/ewm/repository/StatsRepository.java
+++ stats-server/src/main/java/ru/practicum/ewm/repository/StatsRepository.java
@@ -2,7 +2,6 @@ package ru.practicum.ewm.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.practicum.ewm.dto.StatOutDto;
 import ru.practicum.ewm.model.Stat;
 
